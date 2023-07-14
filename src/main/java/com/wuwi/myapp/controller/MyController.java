3ca8492effb152e624a821ce0f4ad1af58814620
WUWI11 <wuwi11@oocl.com>
2023-07-14 16:12:21 +0800

    Initial commit

---
+++ src/main/java/com/wuwi/myapp/controller/MyController.java
@@ -0,0 +1,5 @@
+package com.wuwi.myapp.controller;
+
+public class MyController {
+
+}
---
+++ src/test/java/com/wuwi/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuwi.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
