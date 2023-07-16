2cb6e1172fbeb630f01270acbef0ca7618c45101
wuwi11 <wuwi11@oocl.com>
2023-07-16 23:09:37 +0800

    chore: test

--- src/main/java/com/wuwi/myapp/controller/MyController.java
+++ src/main/java/com/wuwi/myapp/controller/MyController.java
@@ -1,5 +1,13 @@
 package com.wuwi.myapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
 }
